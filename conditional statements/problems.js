842afe57fd2b9f0681be16ff16ddf0cc6b8c4588
dskvamsi <dskvamsi24@gmail.com>
2026-06-17 19:20:52 +0000

    added conditional statements in js

---
+++ conditional statements/problems.js
@@ -0,0 +1,60 @@
+// // 1. Positive or Negative
+
+// let num = -5;
+// if(num >=0){
+//   console.log("Num is positive");
+// } else{
+//     console.log("num is negative");
+// }
+
+
+// 2. Even or Odd 
+
+// let num = 8;
+// if (num %2==0){
+//     console.log("Num is even");
+// } else{
+//     console.log("Num is odd");
+// }
+
+
+// 3. Adult or Minor
+// Create a variable age. If age is 18 or above, print "Adult".
+// Otherwise print "Minor".
+
+
+// let age = 16;
+// if(age >18){
+//     console.log("Adult");
+// } else{
+//     console.log("Minor");
+// }
+
+
+
+// 4. Pass or Fail
+
+// Create a variable marks.
+// If marks are 35 or more, print "Pass". Otherwise print "Fail"
+
+// let marks = 36;
+// if(marks >=35){
+//     console.log("pass");
+// } else{
+//     console.log("fail");
+// }
+
+
+
+
+// 5. Check Password
+
+// Create a variable password.
+// If password equals "admin123", print "Login Successful". Otherwise print "Wrong Password".
+
+let password = "admin123";
+if (password == "admin123") {
+ console.log("Login Successful");
+} else {        
+    console.log("Wrong password");
+}
