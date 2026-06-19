640fdc38d480e27f37d35fb77326899c1b027ce0
dskvamsi <dskvamsi24@gmail.com>
2026-06-19 10:57:03 +0000

    practiced loops in js

---
+++ conditional statements/example.js
@@ -0,0 +1,10 @@
+// Qs1. Get user to input a number using prompt("Enter a number:").
+//  Check if the number is a multiple of 5 or not.
+
+let number = prompt("Enter a number : ");
+if (number %5 ==0){
+    console.log("number is multiple of 5");
+} else {
+        console.log("number isnot multiple of 5");
+
+}
