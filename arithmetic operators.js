9d1040e413a9c3d9f437cb41b62c009ec99c9589
dskvamsi <dskvamsi24@gmail.com>
2026-06-19 22:13:13 +0530

    Delete arithmetic operators.js

--- arithmetic operators.js
+++
@@ -1,8 +0,0 @@
-let a = 5;
-let b = 10;
-console.log("a + b = ", a + b );
-console.log("a - b = ",  a - b);
-console.log("a * b = ",  a * b);
-console.log("a / b = ",  a / b);
-console.log("a % b = ",  a % b);
-console.log("a ** b = ",  a ** b);
