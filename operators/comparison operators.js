777e51a79312726c40afb49b3650584fa443e3f6
dskvamsi <dskvamsi24@gmail.com>
2026-06-19 15:28:06 +0000

    added operators in js

---
+++ operators/comparison operators.js
@@ -0,0 +1,5 @@
+let a = 5;
+let b = 2;
+
+console.log("5 != 2", a==b);
+console.log("5 ")
---
+++ operators/unary operators.js
@@ -0,0 +1,6 @@
+let a = 10;
+let b = 5;
+console.log("a = ", a, " & b = ", b );
+a = a - 2 ;
+console.log(a);
+
